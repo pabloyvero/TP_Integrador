977c67403caf51ddaef134e29d3b2a336d483727
pabloyvero <131806773+pabloyvero@users.noreply.github.com>
2023-07-16 18:22:25 -0300

    Add files via upload

--- inscriptos.php
+++ inscriptos.php
@@ -1,8 +1,19 @@
-<?php
-    //require_once "cabecera.html";
-    echo "Lista de inscriptos";
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Base de Datos</title>
+</head>
+<body>
+    <div>
+        <h1>Oradores Inscriptos</h1>
+        <?php
+            //echo "Lista de inscriptos";
+        
 
-    var_dump($_POST);
+            //var_dump($_POST);
 
     //conexion
 
@@ -26,11 +37,42 @@
         //var_dump($listado);
 
         //var_dump($listado[3]['nombre']);
+        ?>
+            <table class="table table-bordered border-primary">
+                <tr class="table-info">
+                    <th class="table-info">Nombre</th>
+                    <th class="table-info">Apellido</th>
+                    <th class="table-info">Observaciones</th>
+                </tr>
 
+            <?php
         while ($listadoarray = mysqli_fetch_array($consultas)){
             //echo "probando la cantidad";
-            echo $listadoarray['nombre'] . " " . $listadoarray['apellido'];
-            echo "<br>";
+            ?>
+                <tr class="table-info">
+                    <th class="table-info">
+            <?php
+                echo $listadoarray['nombre'];
+            ?>
+                    </th>
+                    <th class="table-info">
+            <?php
+                echo $listadoarray['apellido'];    
+            ?>
+                    </th>
+                    <th class="table-info">
+            <?php
+                echo $listadoarray['memo'];        
+            ?>
+                    </th>
+                </tr>
+            <?php
         }
     }
-?>
+        ?>
+            </table>
+    </div>    
+</body>
+</html>
+
+
